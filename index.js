56b45a2aa4b9faa0c19d2be4dfd48934925a048d
chivalry02 <chivalry02@163.com>
2016-11-21 16:14:45 +0800

    1121

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 require("http").createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('OK');
-}).listen(process.env.PORT | 5000);
+}).listen(process.env.PORT || 5000);
